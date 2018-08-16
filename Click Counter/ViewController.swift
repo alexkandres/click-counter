4fddb8e11d03ccc6eaad65f3f4358b6ee657d529
Alex Andres <alex.k.andres@disney.com>
2018-08-16 09:58:18 -0400

    add decrement button and new label

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     //TODO create 2 params
     var label: UILabel!
+    var labelDuplicate: UILabel!
     var count = 0
     
     override func viewDidLoad() {
@@ -25,21 +26,48 @@ class ViewController: UIViewController {
         view.addSubview(labelWithNumber)
         self.label = labelWithNumber
         
+        //labelDuplicate
+        let labelWithNumberDuplicate = UILabel()
+        labelWithNumberDuplicate.frame = CGRect(x: 180, y: 150, width: 60, height: 60)
+        labelWithNumberDuplicate.text = "0"
+        view.addSubview(labelWithNumberDuplicate)
+        self.labelDuplicate = labelWithNumberDuplicate
+        
         //button
         let buttonForIncrement = UIButton()
-        buttonForIncrement.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        buttonForIncrement.setTitle("Click", for: .normal)
+        buttonForIncrement.frame = CGRect(x: 150, y: 250, width: 100, height: 60)
+        buttonForIncrement.setTitle("Increment", for: .normal)
         buttonForIncrement.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(buttonForIncrement)
         
         //add target action to the button
         buttonForIncrement.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+        
+        //buttonDecrement
+        let buttonForDecrement = UIButton()
+        buttonForDecrement.frame = CGRect(x: 150, y: 350, width: 100, height: 60)
+        buttonForDecrement.setTitle("Decrement", for: .normal)
+        buttonForDecrement.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(buttonForDecrement)
+        
+        //add target action to the button
+        buttonForDecrement.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
     }
     
     //TODO create method that increments and sets new text
     @objc func incrementCount() {
         count += 1
         label.text = "\(count)"
+        labelDuplicate.text = "\(count)"
+
+    }
+    
+    //TODO create method that increments and sets new text
+    @objc func decrementCount() {
+        count -= 1
+        label.text = "\(count)"
+        labelDuplicate.text = "\(count)"
+        
     }
 
 }
